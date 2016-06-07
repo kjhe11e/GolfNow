0b05c020698a7dd12a857ecdd6b82d1c3286711e
Kyle Helle <kjhelle4@gmail.com>
2016-06-07 08:36:44 -0700

    initial commit

---
+++ app/src/main/java/com/example/khelle/golf/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.khelle.golf;
+
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String[] nav_drawer_menu_items;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        nav_drawer_menu_items = getResources().getStringArray(R.array.nav_drawer_menu_items);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        // Set adapter for the list view
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.drawer_list_item, nav_drawer_menu_items));
+
+        // Set list's click listener
+        //mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
+    }
+}
