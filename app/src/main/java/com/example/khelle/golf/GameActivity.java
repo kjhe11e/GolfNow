0993a064b6bbd2c1d4c92cc4e9882eda12498460
Kyle Helle <kjhelle4@gmail.com>
2016-06-07 23:44:14 -0700

    Add Setup and Game activities

--- app/src/main/java/com/example/khelle/golf/GameActivity.java
+++ app/src/main/java/com/example/khelle/golf/GameActivity.java
@@ -1,30 +1,27 @@
 package com.example.khelle.golf;
 
-import android.content.Intent;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+public class GameActivity extends AppCompatActivity {
 
     private String[] nav_drawer_menu_items;
     private DrawerLayout mDrawerLayout;
     private ListView mDrawerList;
-    private Button mainPlayButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_game);
 
         nav_drawer_menu_items = getResources().getStringArray(R.array.nav_drawer_menu_items);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mDrawerList = (ListView) findViewById(R.id.left_drawer);
-        mainPlayButton = (Button) findViewById(R.id.main_play_button);
+
+
 
         // Set adapter for the list view
         mDrawerList.setAdapter(new ArrayAdapter<String>(this,
@@ -33,19 +30,7 @@ public class MainActivity extends AppCompatActivity {
         // Set list's click listener
         //mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
 
-        // Set mainPlayButton's click listener
-        mainPlayButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                enterSetup(v);
-            }
-        });
 
-    }
 
-    // Called when user clicks mainPlayButton
-    public void enterSetup(View view) {
-        Intent intent = new Intent(this, SetupActivity.class);
-        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/khelle/golf/SetupActivity.java
+++ app/src/main/java/com/example/khelle/golf/SetupActivity.java
@@ -9,22 +9,23 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+public class SetupActivity extends AppCompatActivity {
 
     private String[] nav_drawer_menu_items;
     private DrawerLayout mDrawerLayout;
     private ListView mDrawerList;
-    private Button mainPlayButton;
+    private Button mTeeOffButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_setup);
 
         nav_drawer_menu_items = getResources().getStringArray(R.array.nav_drawer_menu_items);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mDrawerList = (ListView) findViewById(R.id.left_drawer);
-        mainPlayButton = (Button) findViewById(R.id.main_play_button);
+        mTeeOffButton = (Button) findViewById(R.id.begin_game_button);
+
 
         // Set adapter for the list view
         mDrawerList.setAdapter(new ArrayAdapter<String>(this,
@@ -33,19 +34,19 @@ public class MainActivity extends AppCompatActivity {
         // Set list's click listener
         //mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
 
-        // Set mainPlayButton's click listener
-        mainPlayButton.setOnClickListener(new View.OnClickListener() {
+
+        // Set TeeOff button's click listener
+        mTeeOffButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                enterSetup(v);
+                startGame(v);
             }
         });
-
     }
 
-    // Called when user clicks mainPlayButton
-    public void enterSetup(View view) {
-        Intent intent = new Intent(this, SetupActivity.class);
+    public void startGame(View view) {
+        Intent intent = new Intent(this, GameActivity.class);
         startActivity(intent);
     }
+
 }
